407f9fdc737adfe1933ace52b7409a0409756bf2
raphaeldonanu <raphael.donanu@konga.com>
2022-02-18 12:07:05 +0100

    Changing logo

--- src/Components/Sidebar/SideBar.js
+++ src/Components/Sidebar/SideBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Icon from '../Utils/Icons/Icons';
 import { SidebarContainer } from './SideBarElements';
 import styles from './SideBar.module.css';
+import img from '../Utils/Img/logo.jpeg'
 
 const SideBar = ({ toggleSidebar, isOpen }) => {
     return (
@@ -11,7 +12,7 @@ const SideBar = ({ toggleSidebar, isOpen }) => {
             </div>
             <div>
                 <ul className={styles.navLinks}>
-                    <li><a href="#home"><Icon name='logo'/></a></li>
+                    <li><a href="#home"><img src={img} alt='logo' width='50px' /></a></li>
                     <li onClick={() => toggleSidebar()}><a href="#about">Who We Are</a></li>
                     <li onClick={() => toggleSidebar()}><a href="#services">Our Services</a></li>
                     <li onClick={() => toggleSidebar()}><a href="#about">Our Projects</a></li>
